3ef6116ec8154a584dfa8438c4156b88a3453a1b
Gazuy <gazuyd@gmail.com>
2023-10-18 14:08:48 +0300

    modified:   PZ_Modul_2_Wek_4.ts

--- PZ_Modul_2_Wek_4.ts
+++ PZ_Modul_2_Wek_4.ts
@@ -22,24 +22,35 @@
 */
 
 class PrintMaсhine{
+    tag: string
     fontSize: number
     color: string
     fontFamily: string
-    constructor (fontSize: number, color: string, fontFamily: string) {
+    
+    constructor (tag: string, fontSize: number, color: string, fontFamily: string) {
+        this.tag = tag
         this.fontSize = fontSize
         this.color = color
         this.fontFamily = fontFamily
+        
     }
     print (str: string) {
-        document.write("<p style=\"font-size:" + this.fontSize + "; color: " + this.color + "; font-family:" + this.fontFamily + "\">" + str + "</p>")
+        document.write(`<${this.tag} style = "font-size: ${this.fontSize}; color: ${this.color}; font-family: ${this.fontFamily}"; > ${str} </${this.tag}>`)
     }
     
     
 }
 
-let qwe = new PrintMaсhine(20, 'red', 'Arial')
+let news_Headline_Style = new PrintMaсhine('h2', 20, 'red', 'CHICAGO')
+let news_Text_Style = new PrintMaсhine('p', 16, 'black', 'fantasy')
+let news_Tags_Style = new PrintMaсhine('p', 14, 'blue', 'new york')
+let news_Data_Style = new PrintMaсhine('h4', 14, 'black', 'wide latin')
 
-    qwe.print('Реализовать класс PrintMaсhine, которой состоит из:')
+
+news_Headline_Style.print('Реализовать класс PrintMaсhine, которой состоит из:')
+news_Text_Style.print('Реализовать класс PrintMaсhine, которой состоит из:')
+news_Tags_Style.print('Реализовать класс PrintMaсhine, которой состоит из:')
+news_Data_Style.print('Реализовать класс PrintMaсhine, которой состоит из:')
 
 
 
@@ -66,30 +77,34 @@ class News {
     heading: string
     text: string
     tags: string[]
-    dateOfPublication: Date
+    dateOfPublication: any
     constructor( heading: string, text: string, tags: string[], dateOfPublication: string) {
        this.heading = heading
        this.text = text
        this.tags = tags
        this.dateOfPublication = new Date(dateOfPublication)
     }
+    prescription () {
+      let today = new Date()
+      if (this.dateOfPublication.toLocaleDateString() == today.toLocaleDateString()) {
+        return 'сегодня'
+      } else if (this.dateOfPublication.valueOf() > (today.valueOf() - 1000 * 60 * 60 * 24 * 7)) {
+        return Math.floor((today.valueOf() - this.dateOfPublication.valueOf()) / (1000 * 60 * 60 * 24)) + ' дней назад'
+      } else {
+        return this.dateOfPublication.toLocaleDateString()
+      }
+    }
     print(){
-        function prescription () {
-            let today = new Date()
-            if (this.dateOfPublication.toLocaleDateString() == today.toLocaleDateString()) {
-              return 'сегодня'
-            } else if (this.dateOfPublication.valueOf() > (today.valueOf() - 1000 * 60 * 60 * 24 * 7)) {
-              return ((today.valueOf() - this.ddateOfPublicationate.valueOf()) / (1000 * 60 * 60 * 24)).toFixed(0) + ' дней назад'
-            } else {
-              return this.dateOfPublication.toLocaleDateString()
-            }
-          }
-          document.write(`<h2>${this.heading}</h2> <h4>${prescription ()}</h4> <p>${this.text}</p> <h5>${this.tags.join(' #')}</h5>`)
-
+      news_Headline_Style.print(this.heading)
+      news_Text_Style.print(this.text)
+      news_Tags_Style.print('Реализовать класс PrintMaсhine, которой состоит из:')
+      news_Data_Style.print(this.dateOfPublication.toString())
     }
 }
-const post = new News('you', 'never', ['walk', 'alone'], '2023-07-29')
-console.log(post.print())
+const post = new News('you', 'never', ['walk', 'alone'], '2023-10-14')
+document.write(post.prescription())
+post.print()
+
 
 
 /*
@@ -107,3 +122,89 @@ console.log(post.print())
 новостей, в которых указан переданный в метод тег).
 Продемонстрировать работу написанных методов.
 */
+
+class NewsFeed {
+  arr: any []
+  constructor (arr: any[]){
+    this.arr = []
+  }
+  get length (){
+    return this.arr.length
+  }
+  print(){
+   this.arr.forEach(el => {
+    el.print()
+   }) 
+  }
+  addNews(news:any){
+    this.arr.push(news)
+  }
+  removalNews(index: number){
+    this.arr.splice(index, 1)
+  }
+  searchTag(tag:string){
+    let arr1
+    this.arr.forEach((el: any, i: number, arr: any[]) => el == tag ? arr1.push(this.arr[i]): arr1)
+    return arr1
+  }
+  sortingNews (){
+    this.arr.sort()
+  }
+}
+// class NewsFeed {
+//   array: infoNews[]
+//   constructor(NewsArray: infoNews[]) {
+//     this.array = NewsArray
+//   }
+//   get count() {
+//     return this.array.length
+//   }
+//   print() {
+//     this.array.forEach(el => {
+//       el.print()
+//     })
+//   }
+// }
+
+// const feed = new NewsFeed([
+//   new infoNews('you', 'never', ['walk', 'alone'], '2023-08-23'),
+//   new infoNews('you2', 'never', ['walk', 'alone'], '2023-08-21'),
+//   new infoNews('you3', 'never', ['walk', 'alone'], '2023-07-24'),
+//   new infoNews('you4', 'never', ['walk', 'alone'], '2023-07-09'),
+// ])
+
+// infoNews.push('you', 'never', ['walk', 'alone'], '2023-08-25')
+// console.log(NewsFeed)
+
+// let index = arr.findIndex(el => el.this.heading == this.heading)
+// infoNews.splice(index, 1)
+// console.log(NewsFeed)
+
+// infoNews.sort(a.date - b.date)
+// console.log(NewsFeed)
+
+// infoNews.filter(this.arrayTags = 'you')
+// console.log(NewsFeed)
+
+  // function makeArmy() {
+  //  let shooters = [] ;
+  
+  //   let i = 0;
+  //   while (i < 10) {
+  //     let shooter = function() { // shooter function
+  //       alert( i ); // should show its number
+  //     };
+  //     shooters.push(shooter);
+  //     i++;
+  //   }
+  
+  //   return shooters;
+  // }
+  
+  // /*
+  // let army = makeArmy();
+  
+  // army[0](); // the shooter number 0 shows 10
+  // army[5](); // and number 5 also outputs 10...
+  // // ... all shooters show 10 instead of their 0, 1, 2, 3...
+  // */
--- dist/PZ_Modul_2_Wek_4.js
+++ dist/PZ_Modul_2_Wek_4.js
@@ -20,18 +20,25 @@
 метода.
 */
 var PrintMaсhine = /** @class */ (function () {
-    function PrintMaсhine(fontSize, color, fontFamily) {
+    function PrintMaсhine(tag, fontSize, color, fontFamily) {
+        this.tag = tag;
         this.fontSize = fontSize;
         this.color = color;
         this.fontFamily = fontFamily;
     }
     PrintMaсhine.prototype.print = function (str) {
-        document.write("<p style=\"font-size:" + this.fontSize + "; color: " + this.color + "; font-family:" + this.fontFamily + "\">" + str + "</p>");
+        document.write("<" + this.tag + " style = \"font-size: " + this.fontSize + "; color: " + this.color + "; font-family: " + this.fontFamily + "\"; > " + str + " </" + this.tag + ">");
     };
     return PrintMaсhine;
 }());
-var qwe = new PrintMaсhine(20, 'red', 'Arial');
-qwe.print('Реализовать класс PrintMaсhine, которой состоит из:');
+var news_Headline_Style = new PrintMaсhine('h2', 20, 'red', 'CHICAGO');
+var news_Text_Style = new PrintMaсhine('p', 16, 'black', 'fantasy');
+var news_Tags_Style = new PrintMaсhine('p', 14, 'blue', 'new york');
+var news_Data_Style = new PrintMaсhine('h4', 14, 'black', 'wide latin');
+news_Headline_Style.print('Реализовать класс PrintMaсhine, которой состоит из:');
+news_Text_Style.print('Реализовать класс PrintMaсhine, которой состоит из:');
+news_Tags_Style.print('Реализовать класс PrintMaсhine, которой состоит из:');
+news_Data_Style.print('Реализовать класс PrintMaсhine, которой состоит из:');
 /*
 Задание 2
 Реализовать класс, описывающий новость (заголовок, текст,
@@ -54,25 +61,29 @@ var News = /** @class */ (function () {
         this.tags = tags;
         this.dateOfPublication = new Date(dateOfPublication);
     }
-    News.prototype.print = function () {
-        function prescription() {
-            var today = new Date();
-            if (this.dateOfPublication.toLocaleDateString() == today.toLocaleDateString()) {
-                return 'сегодня';
-            }
-            else if (this.dateOfPublication.valueOf() > (today.valueOf() - 1000 * 60 * 60 * 24 * 7)) {
-                return ((today.valueOf() - this.ddateOfPublicationate.valueOf()) / (1000 * 60 * 60 * 24)).toFixed(0) + ' дней назад';
-            }
-            else {
-                return this.dateOfPublication.toLocaleDateString();
-            }
+    News.prototype.prescription = function () {
+        var today = new Date();
+        if (this.dateOfPublication.toLocaleDateString() == today.toLocaleDateString()) {
+            return 'сегодня';
         }
-        document.write("<h2>" + this.heading + "</h2> <h4>" + prescription() + "</h4> <p>" + this.text + "</p> <h5>" + this.tags.join(' #') + "</h5>");
+        else if (this.dateOfPublication.valueOf() > (today.valueOf() - 1000 * 60 * 60 * 24 * 7)) {
+            return Math.floor((today.valueOf() - this.dateOfPublication.valueOf()) / (1000 * 60 * 60 * 24)) + ' дней назад';
+        }
+        else {
+            return this.dateOfPublication.toLocaleDateString();
+        }
+    };
+    News.prototype.print = function () {
+        news_Headline_Style.print(this.heading);
+        news_Text_Style.print(this.text);
+        news_Tags_Style.print('Реализовать класс PrintMaсhine, которой состоит из:');
+        news_Data_Style.print(this.dateOfPublication.toString());
     };
     return News;
 }());
-var post = new News('you', 'never', ['walk', 'alone'], '2023-07-29');
-console.log(post.print());
+var post = new News('you', 'never', ['walk', 'alone'], '2023-10-14');
+document.write(post.prescription());
+post.print();
 /*
 Задание 3
 Реализовать класс, описывающий новостную ленту.
@@ -88,3 +99,83 @@ console.log(post.print());
 новостей, в которых указан переданный в метод тег).
 Продемонстрировать работу написанных методов.
 */
+var NewsFeed = /** @class */ (function () {
+    function NewsFeed(arr) {
+        this.arr = [];
+    }
+    Object.defineProperty(NewsFeed.prototype, "length", {
+        get: function () {
+            return this.arr.length;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    NewsFeed.prototype.print = function () {
+        this.arr.forEach(function (el) {
+            el.print();
+        });
+    };
+    NewsFeed.prototype.addNews = function (news) {
+        this.arr.push(news);
+    };
+    NewsFeed.prototype.removalNews = function (index) {
+        this.arr.splice(index, 1);
+    };
+    NewsFeed.prototype.searchTag = function (tag) {
+        var _this = this;
+        var arr1;
+        this.arr.forEach(function (el, i, arr) { return el == tag ? arr1.push(_this.arr[i]) : arr1; });
+        return arr1;
+    };
+    NewsFeed.prototype.sortingNews = function () {
+        this.arr.sort();
+    };
+    return NewsFeed;
+}());
+// class NewsFeed {
+//   array: infoNews[]
+//   constructor(NewsArray: infoNews[]) {
+//     this.array = NewsArray
+//   }
+//   get count() {
+//     return this.array.length
+//   }
+//   print() {
+//     this.array.forEach(el => {
+//       el.print()
+//     })
+//   }
+// }
+// const feed = new NewsFeed([
+//   new infoNews('you', 'never', ['walk', 'alone'], '2023-08-23'),
+//   new infoNews('you2', 'never', ['walk', 'alone'], '2023-08-21'),
+//   new infoNews('you3', 'never', ['walk', 'alone'], '2023-07-24'),
+//   new infoNews('you4', 'never', ['walk', 'alone'], '2023-07-09'),
+// ])
+// infoNews.push('you', 'never', ['walk', 'alone'], '2023-08-25')
+// console.log(NewsFeed)
+// let index = arr.findIndex(el => el.this.heading == this.heading)
+// infoNews.splice(index, 1)
+// console.log(NewsFeed)
+// infoNews.sort(a.date - b.date)
+// console.log(NewsFeed)
+// infoNews.filter(this.arrayTags = 'you')
+// console.log(NewsFeed)
+// function makeArmy() {
+//  let shooters = [] ;
+//   let i = 0;
+//   while (i < 10) {
+//     let shooter = function() { // shooter function
+//       alert( i ); // should show its number
+//     };
+//     shooters.push(shooter);
+//     i++;
+//   }
+//   return shooters;
+// }
+// /*
+// let army = makeArmy();
+// army[0](); // the shooter number 0 shows 10
+// army[5](); // and number 5 also outputs 10...
+// // ... all shooters show 10 instead of their 0, 1, 2, 3...
+// */
